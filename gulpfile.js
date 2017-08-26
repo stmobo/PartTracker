6e60d775315c4993b94fe403656474df22568649
Sebastian Mobo <stmobo@gmail.com>
2017-08-26 13:21:16 -0500

    Rework gulpfile to use watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
-var noop = require('gulp-noop');
+var gutil = require('gulp-util');
 var pump = require('pump');
 
 function react_browserify(infile, outfile, debug) {
   outfile = (outfile === undefined) ? infile : outfile;
   debug = (debug === undefined) ? false : debug;
 
-  return function(cb) {
+  var b = browserify('client-js/'+infile+'.jsx', {transform: 'babelify', debug:true});
+  b = watchify(b);
+
+  function bundlefn(cb) {
     pump([
-        browserify('client-js/'+infile+'.jsx', {transform: 'babelify', debug:true}).bundle(),
+        b.bundle(),
         source(outfile+'.js'),
-        debug ? noop() : streamify(uglify()),
+        debug ? gutil.noop() : streamify(uglify()),
         gulp.dest('static/js')
     ], cb);
   }
+
+  b.on('update', bundlefn);
+  b.on('log', gutil.log);
+
+  return bundlefn;
 }
 
 gulp.task('build-login', react_browserify('login'));
