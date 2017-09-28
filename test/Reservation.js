4204c6fbebd6e1702e73c30a0f0c318e791b2b53
Sebastian Mobo <stmobo@gmail.com>
2017-09-28 16:12:47 -0500

    Add tests for Item/Reservation interaction

--- test/Reservation.js
+++ test/Reservation.js
@@ -13,27 +13,80 @@ should = chai.should();
 var common = require('test/support/model_common.js');
 
 describe('Reservation', function() {
-    var testItemName = 'Foobar';
-    var testString = 'batman';
-    var testCount = 42;
-    var testNumericString = '42';
-
     afterEach(function() {
         /* Completely clear Inventory and Reservation collections. */
         dbAPI.inventory.remove({});
         dbAPI.reservations.remove({});
     });
 
-    describe('#count()', common.numeric_prop_tests.bind(null, dbAPI.reservations, Reservation, 'count'));
-
     describe('#part()', function() {
         common.model_prop_tests(dbAPI.reservations, Reservation, 'part', Item);
+
+        it(`should cause this Reservation's ID to appear using Item.reservations()`, async function () {
+            var itemInstance = new Item();
+            var rsvpInstance = new Reservation();
+
+            await rsvpInstance.part(itemInstance.id());
+            await Promise.all([
+                itemInstance.save(),
+                rsvpInstance.save()
+            ]);
+
+            return itemInstance.reservations().should.eventually.have.lengthOf(1).and.satisfy(
+                x => x[0].equals(rsvpInstance.id()) /* The list's only object has an ID matching rsvpInstance */
+            );
+        });
     });
 
     describe('#requester()', function() {
         common.model_prop_tests(dbAPI.reservations, Reservation, 'requester', User);
     });
 
+    describe('#count()', function () {
+        common.numeric_prop_tests(dbAPI.reservations, Reservation, 'count');
+
+        it(`should increase the Item.reserved() property for associated items`, async function () {
+            var itemCount = 60;
+            var rsvpCount = 10;
+
+            var itemInstance = new Item();
+            var rsvpInstance = new Reservation();
+
+            await itemInstance.count(itemCount);
+            await Promise.all([
+                rsvpInstance.part(itemInstance.id()),
+                rsvpInstance.count(rsvpCount)
+            ]);
+
+            await Promise.all([
+                itemInstance.save(),
+                rsvpInstance.save()
+            ]);
+
+            return itemInstance.reserved().should.become(rsvpCount);
+        });
+
+        it(`should decrease the Item.available() property for associated items`, async function () {
+            var itemCount = 60;
+            var rsvpCount = 10;
+
+            var itemInstance = new Item();
+            var rsvpInstance = new Reservation();
+
+            await itemInstance.count(itemCount);
+            await Promise.all([
+                rsvpInstance.part(itemInstance.id()),
+                rsvpInstance.count(rsvpCount)
+            ]);
+
+            await Promise.all([
+                itemInstance.save(),
+                rsvpInstance.save()
+            ]);
+
+            return itemInstance.available().should.become(itemCount-rsvpCount);
+        });
+    });
 
     describe.skip('#summary()', function() {
 
