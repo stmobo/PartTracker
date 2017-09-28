0a40a6f0ecc916cb3e14af7adac35b54a4486785
Sebastian Mobo <stmobo@gmail.com>
2017-09-28 14:58:50 -0500

    Fix Reservation model to make tests for #count() pass

--- api/models/Reservation.js
+++ api/models/Reservation.js
@@ -11,7 +11,26 @@ var Reservation = function(id) {
 Reservation.prototype = Object.create(dbAPI.DatabaseItem.prototype);
 Reservation.prototype.constructor = Reservation;
 
-Reservation.prototype.count = function(v) { return this.prop('count', v); };
+Reservation.prototype.count = async function(v) {
+    if(v !== undefined) {
+        if(typeof v === 'string' && !isNaN(parseInt(v, 10))) {
+            v = parseInt(v, 10);
+        } else if(typeof v !== 'number') {
+            throw new Error("Parameter to count() must be a number or a numerical string!");
+        }
+
+        return this.prop('count', v);
+    } else {
+        var t = await this.prop('count');
+        if(typeof t === 'string' && !isNaN(parseInt(t, 10))) {
+            return parseInt(t, 10);
+        } else if(typeof t === 'number') {
+            return t;
+        }
+
+        throw new Error("Got non-numerical value for count() from database!");
+    }
+};
 
 Reservation.prototype.requester = function(v) {
     if(v === undefined) {
