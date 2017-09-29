0bdf5543e3ed42a9be1c03b47d56bf0441f7c41a
Sebastian Mobo <stmobo@gmail.com>
2017-09-28 23:55:12 -0500

    Modify InventoryRequest methods to pass tests

--- api/models/InventoryRequest.js
+++ api/models/InventoryRequest.js
@@ -4,6 +4,8 @@ var dbAPI = require('api/db.js');
 var Item = require('api/models/Item.js');
 var User = require('api/models/User.js');
 
+var type = require('type-detect');
+
 var InventoryRequest = function (id) {
     dbAPI.DatabaseItem.call(this, dbAPI.requests, id);
 };
@@ -12,34 +14,58 @@ InventoryRequest.prototype = Object.create(dbAPI.DatabaseItem.prototype);
 InventoryRequest.prototype.constructor = InventoryRequest;
 
 InventoryRequest.prototype.item = async function(v) {
-    if(v === undefined) return this.prop('item');
-    var targetItem = new Item(monk.id(v));
+    if(v === undefined) {
+        var id = await this.prop('item');
+        if(id === null) return null;
 
-    if(await targetItem.exists()) {
-        return this.prop('item', monk.id(v));
-    } else {
-        throw new Error("Requested item does not exist!");
+        return new Item(id);
     }
+
+    var targetItem;
+    if(v instanceof Item) {
+        targetItem = v;
+    } else if(v instanceof ObjectID || type(v) === 'string') {
+        targetItem = new Item(monk.id(v));
+    } else {
+        throw new Error("Invalid value passed to #item()!");
+    }
+
+    return this.prop('item', targetItem.id());
 };
 
 InventoryRequest.prototype.requester = async function(v) {
-    if(v === undefined) return this.prop('requester');
-    var targetUser = new User(monk.id(v));
-
-    if(await targetUser.exists()) {
-        return this.prop('requester', monk.id(v));
-    } else {
-        throw new Error("Requesting user does not exist!");
+    if(v === undefined) {
+        var id = await this.prop('requester');
+        if(id === null) return null;
+        
+        return new User(id);
     }
+
+    var targetUser;
+    if(v instanceof User) {
+        targetUser = v;
+    } else if(v instanceof ObjectID || type(v) === 'string') {
+        targetUser = new User(monk.id(v));
+    } else {
+        throw new Error("Invalid value passed to #requester()!");
+    }
+
+    return this.prop('requester', targetUser.id());
 }
 
-InventoryRequest.prototype.count = function(v) {
-    if(typeof v === 'string' && !isNaN(parseInt(v))) {
-        return this.prop('count', parseInt(v));
-    } else if(typeof v === 'number') {
+InventoryRequest.prototype.count = async function(v) {
+    if(type(v) === 'string' && !isNaN(parseInt(v, 10))) {
+        return this.prop('count', parseInt(v, 10));
+    } else if(type(v) === 'number') {
         return this.prop('count', v);
     } else if(v === undefined) {
-        return this.prop('count');
+        var t = await this.prop('count');
+        if(t === null || type(t) === 'number')
+            return t;
+        else if(type(t) === 'string' && !isNaN(parseInt(t, 10)))
+            return parseInt(t, 10);
+        else
+            throw new Error("Got invalid number for #count() from database!");
     } else {
         throw new Error("Invalid parameter for requested count!");
     }
@@ -56,7 +82,15 @@ InventoryRequest.prototype.status = async function(v) {
 }
 
 InventoryRequest.prototype.eta = async function(v) {
-    if(v === undefined) return this.prop('eta');
+    if(v === undefined) {
+        var eta_str = await this.prop('eta');
+        if(eta_str === null) return null;
+
+        var t = new Date(eta_str);
+        if(!isNaN(t.getTime())) return t;
+
+        throw new Error('Retrieved date for #eta() from database is not valid!');
+    }
 
     var targetDate = new Date(v);
     if(!isNaN(targetDate.getTime())) {
