e3b9b8770f8c09b5313cc67dc840e7b3072ab98e
Sebastian Mobo <stmobo@gmail.com>
2017-10-05 21:42:43 -0500

    Set all pages to render after persistence rehydrate

--- client-js/users.jsx
+++ client-js/users.jsx
@@ -6,16 +6,16 @@ import { Provider } from 'react-redux';
 
 import UserList from './users/UserList.jsx';
 
-import { store } from './common/store.js';
+import { store, persist } from './common/store.js';
 import api from './common/api.js';
 
-
-ReactDOM.render(
-    <Provider store={store}>
-        <UserList />
-    </Provider>,
-    document.getElementById('root')
+persist.then(
+    (store) => {
+        ReactDOM.render(
+            <Provider store={store}>
+                <UserList />
+            </Provider>,
+            document.getElementById('root')
+        );
+    }
 );
-
-store.dispatch(api.readCollection('users'));
-store.dispatch(api.getCurrentUser());
