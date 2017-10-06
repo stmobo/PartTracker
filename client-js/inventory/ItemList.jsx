510e5a8cc01371891dabfc0eef5e0d3f48e1c7c2
Sebastian Mobo <stmobo@gmail.com>
2017-10-05 21:26:10 -0500

    Add sort functionality to inventory screen

--- client-js/inventory/ItemList.jsx
+++ client-js/inventory/ItemList.jsx
@@ -6,37 +6,87 @@ import ItemCreateForm from "./ItemCreateForm.jsx";
 import FileUploadButton from '../common/FileUploadButton.jsx';
 import api from "../common/api.js";
 
-function ItemListHeader() {
+function ItemListHeader({ setSortKey, sortState }) {
+
+    if(sortState.sortInverted) {
+        var sortArrow = (<span className="glyphicon glyphicon-menu-up text-left"></span>);
+    } else {
+        var sortArrow = (<span className="glyphicon glyphicon-menu-down text-left"></span>);
+    }
+
     return (
         <div className="list-header row">
-            <div className="col-md-6"><strong>Item Name</strong></div>
-            <div className="col-md-2"><strong>Status</strong></div>
-            <div className="col-md-1"><strong>Available</strong></div>
-            <div className="col-md-1"><strong>Requested</strong></div>
-            <div className="col-md-1"><strong>Reserved</strong></div>
-            <div className="col-md-1"><strong>Total</strong></div>
+            <div className="col-md-6" onClick={setSortKey.bind(null, 'name')}>
+                {sortState.sortKey === 'name' && sortArrow}<strong>Item Name</strong>
+            </div>
+            <div className="col-md-2" onClick={setSortKey.bind(null, 'status')}>
+                {sortState.sortKey === 'status' && sortArrow}<strong>Status</strong>
+            </div>
+            <div className="col-md-1" onClick={setSortKey.bind(null, 'available')}>
+                {sortState.sortKey === 'available' && sortArrow}<strong>Available</strong>
+            </div>
+            <div className="col-md-1" onClick={setSortKey.bind(null, 'requested')}>
+                {sortState.sortKey === 'requested' && sortArrow}<strong>Requested</strong>
+            </div>
+            <div className="col-md-1" onClick={setSortKey.bind(null, 'reserved')}>
+                {sortState.sortKey === 'reserved' && sortArrow}<strong>Reserved</strong>
+            </div>
+            <div className="col-md-1" onClick={setSortKey.bind(null, 'count')}>
+                {sortState.sortKey === 'count' && sortArrow}<strong>Total</strong>
+            </div>
         </div>
     );
 }
 
-function ItemList({ collection, createItem, updateItem, deleteItem, importCSV }) {
-    var elements = collection.map(
-        x => (<Item key={x.id} itemModel={x} onUpdate={updateItem} onDelete={deleteItem} />)
-    );
+class ItemList extends React.Component {
+    constructor(props) {
+        var { collection, createItem, updateItem, deleteItem, importCSV } = props;
+        super(props);
 
-    return (
-        <div className="container-fluid" id="inv-table">
-            <ItemListHeader />
-            {elements}
-            <ItemCreateForm createItem={createItem} />
-            <div className="list-row row">
-                <div className="col-md-12">
-                    <FileUploadButton accept=".csv" className="btn btn-default btn-sm list-create-new-button" onFileSelected={importCSV}>Import from CSV</FileUploadButton>
-                    <a className="btn btn-default btn-sm list-create-new-button" href="/api/inventory.csv">Export to CSV</a>
+        this.state = { sortKey: null, sortInverted: false };
+        this.setSortKey = (function(key, ev) {
+            ev.stopPropagation();
+            if(this.state.sortKey === key) {
+                this.setState({sortInverted: !this.state.sortInverted});
+            } else {
+                this.setState({sortKey: key, sortInverted: false});
+            }
+        }).bind(this);
+    }
+
+    render() {
+        var { collection, createItem, updateItem, deleteItem, importCSV } = this.props;
+
+        var renderCol = collection;
+        if(this.state.sortKey !== null) {
+            renderCol = collection.slice();
+            renderCol.sort((a,b) => {
+                if(a[this.state.sortKey] < b[this.state.sortKey]) return -1;
+                if(a[this.state.sortKey] === b[this.state.sortKey]) return 0;
+                return 1;
+            });
+
+            if(this.state.sortInverted) renderCol.reverse();
+        }
+
+        var elements = renderCol.map(
+            x => (<Item key={x.id} itemModel={x} onUpdate={updateItem} onDelete={deleteItem} />)
+        );
+
+        return (
+            <div className="container-fluid" id="inv-table">
+                <ItemListHeader setSortKey={this.setSortKey} sortState={this.state} />
+                {elements}
+                <ItemCreateForm createItem={createItem} />
+                <div className="list-row row">
+                    <div className="col-md-12">
+                        <FileUploadButton accept=".csv" className="btn btn-default btn-sm list-create-new-button" onFileSelected={importCSV}>Import from CSV</FileUploadButton>
+                        <a className="btn btn-default btn-sm list-create-new-button" href="/api/inventory.csv">Export to CSV</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 function mapStateToProps(state) {
