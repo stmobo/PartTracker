99691208ec66b4f9ebb6062d5a76e159250b34c2
Sebastian Mobo <stmobo@gmail.com>
2017-10-13 11:59:54 -0500

    Adjust RequestList to work with SortableCollection

--- client-js/requests/RequestList.jsx
+++ client-js/requests/RequestList.jsx
@@ -1,122 +1,64 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import api from '../common/api.js';
 import {store} from '../common/store.js';
+
+import SortableCollection from '../common/SortableCollection.jsx';
+import ListHeaderColumn from '../common/ListHeaderColumn.jsx';
 
 import RequestCreator from './RequestCreator.jsx';
 import Request from './Request.jsx';
 
 function RequestListHeader({ setSortKey, sortState }) {
-    if(sortState.sortReversed) {
-        var sortArrow = (<span className="glyphicon glyphicon-menu-up text-left"></span>);
-    } else {
-        var sortArrow = (<span className="glyphicon glyphicon-menu-down text-left"></span>);
-    }
-
     return (
         <div className="list-header row">
-            <strong className="col-md-4" onClick={setSortKey.bind(null, 'item')}>
-                {sortState.sortKey === 'item' && sortArrow}
+            <ListHeaderColumn setSortKey={setSortKey} sortState={sortState} className="col-md-4" sortKey='item'>
                 Item
-            </strong>
-            <strong className="col-md-1" onClick={setSortKey.bind(null, 'count')}>
-                {sortState.sortKey === 'count' && sortArrow}
+            </ListHeaderColumn>
+            <ListHeaderColumn setSortKey={setSortKey} sortState={sortState} className="col-md-1" sortKey='count'>
                 Count
-            </strong>
-            <strong className="col-md-1" onClick={setSortKey.bind(null, 'status')}>
-                {sortState.sortKey === 'status' && sortArrow}
+            </ListHeaderColumn>
+            <ListHeaderColumn setSortKey={setSortKey} sortState={sortState} className="col-md-1" sortKey='status'>
                 Status
-            </strong>
-            <strong className="col-md-3" onClick={setSortKey.bind(null, 'requester')}>
-                {sortState.sortKey === 'requester' && sortArrow}
+            </ListHeaderColumn>
+            <ListHeaderColumn setSortKey={setSortKey} sortState={sortState} className="col-md-3" sortKey='requester'>
                 Requester
-            </strong>
-            <strong className="col-md-3" onClick={setSortKey.bind(null, 'eta')}>
-                {sortState.sortKey === 'eta' && sortArrow}
+            </ListHeaderColumn>
+            <ListHeaderColumn setSortKey={setSortKey} sortState={sortState} className="col-md-3" sortKey='eta'>
                 ETA
-            </strong>
+            </ListHeaderColumn>
         </div>
     );
 }
 
-function mapStateToProps(state, ownProps) {
-    return {
-        collection: Array.from(state.requests.values()),
+function RequestComparer(sortKey, a, b) {
+    var nA = a[sortKey];
+    var nB = b[sortKey];
+
+    switch(sortKey) {
+        case 'item':
+            nA = store.getState().inventory.get(nA).name.toLowerCase();
+            nB = store.getState().inventory.get(nB).name.toLowerCase();
+            break;
+        case 'requester':
+            nA = store.getState().users.get(nA).realname.toLowerCase();
+            nB = store.getState().users.get(nB).realname.toLowerCase();
+            break;
+        case 'count':
+            nA = parseInt(nA, 10);
+            nB = parseInt(nB, 10);
+            break;
+        case 'status':
+            nA = nA.toLowerCase();
+            nB = nB.toLowerCase();
+            break;
+        case 'eta':
+            nA = (new Date(nA)).getTime();
+            nB = (new Date(nB)).getTime();
+            break;
     }
+
+    if(nA < nB) return -1;
+    if(nA === nB) return 0;
+    return 1;
 }
 
-class RequestList extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            sortKey: '',
-            sortReversed: false
-        }
-
-        this.setSortKey = (function(k) {
-            this.setState({
-                sortKey: k,
-                sortReversed: (this.state.sortKey === k) ? !this.state.sortReversed : false
-            });
-        }).bind(this);
-    }
-
-    render() {
-        var { collection } = this.props;
-
-        var renderCol = collection;
-        if(this.state.sortKey !== '') {
-            renderCol = collection.slice();
-
-            renderCol.sort((a,b) => {
-                var nA = a[this.state.sortKey];
-                var nB = b[this.state.sortKey];
-
-                switch(this.state.sortKey) {
-                    case 'item':
-                        nA = store.getState().inventory.get(nA).name.toLowerCase();
-                        nB = store.getState().inventory.get(nB).name.toLowerCase();
-                        break;
-                    case 'requester':
-                        nA = store.getState().users.get(nA).realname.toLowerCase();
-                        nB = store.getState().users.get(nB).realname.toLowerCase();
-                        break;
-                    case 'count':
-                        nA = parseInt(nA, 10);
-                        nB = parseInt(nB, 10);
-                        break;
-                    case 'status':
-                        nA = nA.toLowerCase();
-                        nB = nB.toLowerCase();
-                        break;
-                    case 'eta':
-                        nA = (new Date(nA)).getTime();
-                        nB = (new Date(nB)).getTime();
-                        break;
-                }
-
-                if(nA < nB) return -1;
-                if(nA === nB) return 0;
-                return 1;
-            });
-
-            if(this.state.sortReversed) renderCol.reverse();
-        }
-
-        var elements = renderCol.map(
-            x => (<Request key={x.id} model={x} />)
-        );
-
-        return (
-            <div className="container-fluid">
-                <RequestListHeader setSortKey={this.setSortKey} sortState={this.state} />
-                {elements}
-                <RequestCreator />
-            </div>
-        );
-    }
-}
-
-RequestList = connect(mapStateToProps)(RequestList);
-export default RequestList;
+export default SortableCollection('requests', Request, RequestCreator, RequestListHeader, RequestComparer);
